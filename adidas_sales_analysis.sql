25862b3cafd43ac920e0512795995e21c038e44c
MOHAMMAD SHAHBAZ ALAM <alam.shahbaz01@gmail.com>
2023-10-07 06:52:54 +0530

    Update adidas_sales_analysis.sql

--- adidas_sales_analysis.sql
+++ adidas_sales_analysis.sql
@@ -8,6 +8,9 @@ SELECT
 	*
 FROM
 	dbo.['Data Sales Adidas$'];
+
+
+
 	
 
 */
@@ -101,3 +104,86 @@ GROUP BY
 	Retailer
 ORDER BY
 	[Total Sales] DESC;
+
+-- What is the monthly average operating margin for Adidas products?
+SELECT
+	MONTH([Invoice Date]) 'Month of Invoice'
+	,AVG([Operating Profit]) 'Avg Operating Margin'
+FROM
+	dbo.['Data Sales Adidas$']
+GROUP BY 
+	MONTH([Invoice Date])
+ORDER BY
+	'Month of Invoice';
+
+-- Which products had the highest total profit in 2021?
+
+SELECT TOP 1
+	Product
+	,SUM([Operating Profit]) 'Total Profit'
+FROM
+	dbo.['Data Sales Adidas$']
+WHERE
+	YEAR([Invoice Date]) = '2021'
+GROUP BY
+	Product
+ORDER BY
+	'Total Profit' DESC;
+
+-- What is the monthly growth rate in total sales revenue?
+
+SELECT 
+	MONTH([Invoice Date]) 'Month of Invoice'
+	,SUM([Total Sales])   'Total Sales'
+	,LAG(SUM([Total Sales])) OVER(ORDER BY MONTH([Invoice Date])) 'Previous Month Sales'
+FROM
+	dbo.['Data Sales Adidas$']
+GROUP BY 
+	MONTH([Invoice Date]);
+
+-- What is the total number of units sold for each products
+SELECT 
+	 Product
+	,COUNT(*) 'Total Products Sold'
+FROM
+	dbo.['Data Sales Adidas$']
+GROUP BY
+	Product
+ORDER BY
+	'Total Products Sold' DESC;
+
+-- Which month had the highest total sales revenue in 2021?
+
+SELECT TOP 1
+	MONTH([Invoice Date]) 'Month of Invoice'
+	,[Total Sales]
+FROM
+	dbo.['Data Sales Adidas$']
+WHERE 
+	YEAR([Invoice Date]) = '2021'
+ORDER BY
+	[Total Sales] DESC;
+
+--  Identify the top-selling product category for each region.
+
+WITH top_selling_product AS (
+	SELECT 
+		Region
+		,Product
+		,SUM([Total Sales]) 'Total Sales'
+		,ROW_NUMBER()OVER(PARTITION BY Region ORDER BY SUM([Total Sales]) DESC) RN
+	FROM
+		dbo.['Data Sales Adidas$']
+	GROUP BY
+		Region
+		,Product
+		)
+SELECT 
+	Region
+	,Product
+FROM	
+	top_selling_product
+WHERE 
+	RN = 1;
+
+
